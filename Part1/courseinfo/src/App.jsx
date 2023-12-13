15dbd522e8631725cff0931b061cde7f745ff14f
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-12-13 16:20:51 -0500

    adding 1.3, -1.5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -29,12 +29,20 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
     ]
   }
-
   return (
     <div>
       <Header course={course.name} />
