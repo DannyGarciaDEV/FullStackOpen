06cbc7558512080ccd40695f49499744b3b8edb9
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-12-14 16:14:49 -0500

    adding exercises 1.1 and 1.2 crearing a simple webapp about the course information

---
+++ Part1/courseinfo/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Header = ({ course }) => (
+  <h1>{course}</h1>
+);
+
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.name} part={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  );
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+export default App;
