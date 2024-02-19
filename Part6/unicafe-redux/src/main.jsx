a5a8dd00fd7700278e706c3cccff647d2536b467
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-02-19 18:55:47 -0500

    removing component 6.2

--- Part6/unicafe-redux/src/main.jsx
+++ Part6/unicafe-redux/src/main.jsx
@@ -1,53 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducer from './reducer'
+import App from './App'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
+const store = createStore(reducer)
 
-const store = createStore(counterReducer)
-
-const App = () => {
-  return (
-    <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button 
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button 
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
