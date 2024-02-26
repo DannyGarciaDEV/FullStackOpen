de22cdf5d83754a08401d71d7fc9807555d2ec19
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-02-26 12:30:05 -0500

    fishing project 6.24

--- Part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,48 +1,77 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { addVote } from '../reducers/anecdoteReducer'
-import { setNotification } from '../reducers/notificationReducer'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addVote } from '../reducers/anecdoteReducer';
+import { setNotification } from '../reducers/notificationReducer';
 
 const AnecdotesList = () => {
-  const anecdotes = useSelector(state => state.anecdotes)
-  const query = useSelector(state => state.query)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.anecdotes);
+  const query = useSelector(state => state.query);
+  const dispatch = useDispatch();
 
   const voteHandler = anecdote => {
-    dispatch(addVote(anecdote))
-    dispatch(setNotification(`You voted for${anecdote.content}`))
-  }
-  let sortedAnecdotes = [...anecdotes]
+    dispatch(addVote(anecdote));
+    dispatch(setNotification(`You voted for ${anecdote.content}`));
+  };
+
+  let sortedAnecdotes = [...anecdotes];
 
   const sortArray = (a, b) => {
     if (a.votes > b.votes) {
-      return -1
+      return -1;
     }
     if (a.votes < b.votes) {
-      return 1
+      return 1;
     } else {
-      return 0
+      return 0;
     }
-  }
-  sortedAnecdotes.sort(sortArray)
+  };
+  sortedAnecdotes.sort(sortArray);
 
   let filteredArray = sortedAnecdotes.filter(anecdote =>
     anecdote.content.toLowerCase().includes(query)
-  )
+  );
 
   return (
     <>
       {filteredArray.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
+        <div key={anecdote.id} style={styles.anecdote}>
+          <div style={styles.content}>{anecdote.content}</div>
+          <div style={styles.vote}>
             has {anecdote.votes}
-            <button onClick={() => voteHandler(anecdote)}>vote</button>
+            <button onClick={() => voteHandler(anecdote)} style={styles.button}>
+              vote
+            </button>
           </div>
         </div>
       ))}
     </>
-  )
-}
+  );
+};
 
-export default AnecdotesList
+const styles = {
+  anecdote: {
+    marginBottom: '15px',
+    border: '1px solid #ccc',
+    padding: '10px',
+    borderRadius: '5px',
+  },
+  content: {
+    fontSize: '18px',
+    marginBottom: '5px',
+  },
+  vote: {
+    fontSize: '16px',
+  },
+  button: {
+    marginLeft: '10px',
+    padding: '5px 10px',
+    fontSize: '14px',
+    borderRadius: '3px',
+    cursor: 'pointer',
+    background: '#007bff',
+    color: '#fff',
+    border: 'none',
+  },
+};
+
+export default AnecdotesList;
--- Part6/redux-anecdotes/src/components/Filter.jsx
+++ Part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,27 +1,43 @@
-import { connect } from 'react-redux'
-import { filterState } from '../reducers/filterReducer'
+import React from 'react';
+import { connect } from 'react-redux';
+import { filterState } from '../reducers/filterReducer';
 
-const Filter = props => {
-  const handleChange = event => {
+const Filter = (props) => {
+  const handleChange = (event) => {
     const {
       target: { value },
-    } = event
+    } = event;
 
-    props.filterState(value.toLowerCase())
-  }
+    props.filterState(value.toLowerCase());
+  };
+
   const style = {
     marginBottom: 10,
-  }
+    fontSize: '16px',
+  };
+
+  const inputStyle = {
+    padding: '8px',
+    fontSize: '14px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+  };
 
   return (
     <div style={style}>
-      filter <input onChange={handleChange} />
+      <label htmlFor="filterInput">Filter</label>{' '}
+      <input
+        id="filterInput"
+        style={inputStyle}
+        onChange={handleChange}
+        placeholder="Type to filter"
+      />
     </div>
-  )
-}
+  );
+};
 
-const mapDispatchToProps = { filterState }
+const mapDispatchToProps = { filterState };
 
-const connectedFilter = connect(null, mapDispatchToProps)(Filter)
+const connectedFilter = connect(null, mapDispatchToProps)(Filter);
 
-export default connectedFilter
+export default connectedFilter;
