54fb2c681e0a2adedd79e110ea8dba2f2676d187
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-02-26 12:18:26 -0500

    finalizing App into main to fix bugs

--- Part6/redux-anecdotes/src/App.jsx
+++ Part6/redux-anecdotes/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import AnecdoteForm from './components/AnecdoteForm'; 
-import AnecdoteList from './components/AnecdoteList'; 
+import { useEffect } from 'react'
+
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdotesList from './components/AnecdoteList'
+import Filter from './components/Filter'
+import Notification from './components/Notification'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      <AnecdoteList />
-      <AnecdoteForm />
-    </div>
-  );
-};
+  const dispatch = useDispatch()
 
-export default App;
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      
+      <AnecdoteForm />
+      <AnecdotesList />
+    </>
+  )
+}
+
+export default App
--- Part6/redux-anecdotes/src/main.jsx
+++ Part6/redux-anecdotes/src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.render(
-  
-    <App />, document.getElementById('root')
-);
+import { Provider } from 'react-redux'
+import App from './App'
+
+import store from './reducers/store'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
