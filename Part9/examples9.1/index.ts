93d4c3b440ca4ac85b8a04eed8b4884f5233bb72
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-26 12:58:52 -0400

    9.4 to 9.5

--- Part9/examples9.1/index.ts
+++ Part9/examples9.1/index.ts
@@ -13,7 +13,7 @@ app.get('/bmi', (req, res) => {
     const height = Number(req.query.height);
     const weight = Number(req.query.weight);
 
-  // Check if height or weight is not a valid number
+  
   if (isNaN(height) || isNaN(weight)) {
     return res.status(400).json({ error: 'malformatted parameters' });
   }
